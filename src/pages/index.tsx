03252b8f8206d5e542af0062cc597a469562c8fd
iamryanmacdonald <ram_idts@me.com>
2022-07-12 09:48:56 +1000

    only show you questions you made

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const QuestionCreator: React.FC = () => {
   const { mutate, isLoading } = trpc.useMutation("questions.create", {
     onSuccess: (data) => {
       console.log("did we succeed?", data);
-      client.invalidateQueries("questions.get-all");
+      client.invalidateQueries("questions.get-all-my-questions");
       if (!inputRef.current) return;
       inputRef.current.value = "";
     },
@@ -32,7 +32,7 @@ const QuestionCreator: React.FC = () => {
 };
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["questions.get-all"]);
+  const { data, isLoading } = trpc.useQuery(["questions.get-all-my-questions"]);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
