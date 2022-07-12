33d931b26afa588165b6df008ec8cb91a553e02a
iamryanmacdonald <ram_idts@me.com>
2022-07-12 12:20:08 +1000

    tailwind forms

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,32 +1,60 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 import { trpc } from "../utils/trpc";
 
-const QuestionCreator: React.FC = () => {
-  const inputRef = React.useRef<HTMLInputElement>(null);
-  const client = trpc.useContext();
-
+const CreateQuestionForm = () => {
   const { mutate, isLoading } = trpc.useMutation("questions.create", {
-    onSuccess: (data) => {
-      console.log("did we succeed?", data);
-      client.invalidateQueries("questions.get-all-my-questions");
-      if (!inputRef.current) return;
-      inputRef.current.value = "";
-    },
+    onSuccess: (data) => {},
   });
 
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    watch,
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+  console.log(watch("example"));
+
   return (
-    <input
-      ref={inputRef}
-      disabled={isLoading}
-      onKeyDown={(event) => {
-        if (event.key === "Enter") {
-          console.log("enter!!!", event.currentTarget.value);
-          mutate({ question: event.currentTarget.value });
-        }
-      }}
-    ></input>
+    <div className="antialiased px-6 text-gray-100">
+      <div className="max-w-xl mx-auto py-12 md:max-w-4xl">
+        <h2 className="font-bold text-2xl">Reset styles</h2>
+        <p className="mt-2 text-gray-300 text-lg">
+          These are form elements this plugin styles by default.
+        </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="gap-6 grid grid-cols-1 items-start mt-8 md:grid-cols-2">
+            <div className="col-span-2 gap-6 grid grid-cols-1">
+              <label className="block">
+                <span className="text-gray-200">Question</span>
+                <input
+                  type="text"
+                  className="block form-input mt-1 text-gray-800 w-full"
+                  placeholder="How do magnets work?"
+                />
+              </label>
+            </div>
+            <div className="col-span-2 gap-6 grid grid-cols-1">
+              <label className="block">
+                <input
+                  type="submit"
+                  className="form-input text-gray-900"
+                  value="Create question"
+                />
+              </label>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
+};
+
+const QuestionCreator: React.FC = () => {
+  return <CreateQuestionForm />;
 };
 
 export default QuestionCreator;
